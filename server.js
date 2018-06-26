b5cf303e93a464265a2d46aaadfb8f0e42a56276
Sirisha Malarapu <msirisha@athenahealth.com>
2018-06-26 18:57:32 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+
+app.use((req, resp, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if (error) {
+            console.log('Unable to append log to the file');
+        }
+    });
+    next();
+});
+
+app.use((req, resp, next) => {
+    resp.render('maintenance.hbs');
+})
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+
+app.get('/', (req, resp) => {
+    resp.render('home.hbs', {
+        pageTitle: 'Home Page',
+        name: 'Sirisha'
+    });
+});
+
+app.get('/about', (req, resp) => {
+    resp.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, resp) => {
+    resp.send({
+        errorMessage: 'Unable to handle request'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
